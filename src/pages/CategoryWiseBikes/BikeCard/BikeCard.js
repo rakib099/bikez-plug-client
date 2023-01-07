68b1c26eb6a586305004d2edb24a108618ac1beb
Rakib099 <rakibnabi97@gmail.com>
2023-01-08 01:40:11 +0600

    Category/:id page data loaded

--- src/pages/CategoryWiseBikes/BikeCard/BikeCard.js
+++ src/pages/CategoryWiseBikes/BikeCard/BikeCard.js
@@ -1,48 +1,49 @@
 import React from 'react';
-import kidsBike from '../../../assets/images/kids-bike-3.png';
-import { HiShieldCheck } from 'react-icons/hi';
 import { BsCheckCircleFill } from 'react-icons/bs';
+import getFormattedToday from '../../../utils/getFormattedToday';
 
-const BikeCard = () => {
-    const today = new Date();
-    const yyyy = today.getFullYear();
-    let mm = today.getMonth() + 1; // Months start at 0!
-    let dd = today.getDate();
-
-    if (dd < 10) dd = '0' + dd;
-    if (mm < 10) mm = '0' + mm;
-
-    const formattedToday = dd + '/' + mm + '/' + yyyy;
+const BikeCard = ({bike}) => {
+    const {name, img, condition, resalePrice, originalPrice, 
+        purchaseYear, location, postedOn, seller} = bike;
+    
+    const currentYear = new Date().getFullYear();
+    const yearsUsed = currentYear - purchaseYear;
 
     return (
-        <div className="card bg-base-100 shadow-xl">
-            <figure><img src={kidsBike} alt="Bike" className='max-w-full h-auto' /></figure>
-            <div className="card-body">
+        <div className="card bg-base-100 shadow-[3px_4px_10px_2px_rgba(0,0,0,0.05)]">
+            <figure><img src={img} alt="Bike" className='max-w-full h-auto' /></figure>
+            <div className="card-body p-6">
                 <div className="">
                     <h2 className="card-title">
-                        Girls Baby Bicycle Blue
+                        {name}
                     </h2>
                     <div className="flex justify-between">
                         <p className="flex items-center gap-2">
-                            <span className='text-gray-500'>Elena Gilbert</span>
+                            <span className='text-gray-500'>{seller}</span>
                             <BsCheckCircleFill title='Verified Seller' className='text-primary text-lg cursor-pointer' />
                         </p>
-                        <button className="btn btn-sm btn-secondary hover:btn-primary">Book now</button>
+
                     </div>
                 </div>
-                <div className="bg-[#E8F4FF] p-2 rounded-lg">
-                    <div className="flex justify-between">
-                        <p>Condition: <span className='text-gray-400 font-semibold'>Excellent</span></p>
-                        <p className='text-end'>Resale Price: <span className='text-gray-400 font-semibold'>$100</span></p>
+                <div className="bg-[#E8F4FF] py-3 px-3 rounded-lg mb-2">
+                    <div className="flex justify-between mb-3">
+                        <p>Condition: <span className={`text-teal-600 font-semibold`}>{condition}</span></p>
+                        <p className='text-end'>Resale Price: <span className=' text-sky-600 font-semibold'>${resalePrice}</span></p>
                     </div>
-                    <div className="flex justify-between">
-                        <p>Used: <span className='text-gray-400 font-semibold'>{'04'} years</span></p>
-                        <p className='text-end'>Original Price: <span className='text-gray-400 font-semibold'>$150</span></p>
+                    <div className="flex justify-between mb-4">
+                        <p>Used: <span className='text-gray-400 font-semibold'>{yearsUsed} years</span></p>
+                        <p className='text-end'>Original Price: <span className='text-lime-500 font-semibold'>${originalPrice}</span></p>
                     </div>
+                    <div className="card-actions justify-between">
+                        <div className="badge badge-outline">
+                            {location}
+                        </div>
+                        <div className="badge badge-outline">{postedOn}</div>
+                    </div>
+                    
                 </div>
-                <div className="card-actions justify-between mt-4">
-                    <div className="badge badge-outline">{"Jatrabari, Dhaka, Bangladesh"}</div>
-                    <div className="badge badge-outline">{formattedToday}</div>
+                <div className="text-center">
+                    <button className="btn btn-sm btn-secondary hover:btn-primary px-5">Book now</button>
                 </div>
             </div>
         </div>
--- src/pages/CategoryWiseBikes/CategoryWiseBikes/CategoryWiseBikes.js
+++ src/pages/CategoryWiseBikes/CategoryWiseBikes/CategoryWiseBikes.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import BikeCard from '../BikeCard/BikeCard';
+import { useQuery } from '@tanstack/react-query';
+import Spinner from '../../../components/Spinner/Spinner';
 
 const CategoryWiseBikes = () => {
-    const title = useLoaderData();
-    console.log(title);
+    const category = useLoaderData();
+    const { _id, title } = category;
+    console.log(title)
+
+    const { data: bikes, isLoading } = useQuery({
+        queryKey: ['bikes', _id],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/bikes/category/${_id}`);
+            const data = await res.json();
+            return data;
+        }
+    })
+
+    if (isLoading) {
+        return <Spinner />;
+    }
+
 
     return (
-        <div className='mx-5 mt-3 mb-10'>
-            <h3 className="text-2xl font-semibold text-center">
-                {title.title}
+        <div className={`px-5 lg:px-8 mt-3 pb-20 pt-3`}>
+            <h3 className="text-3xl font-semibold text-center mb-7">
+                {title}
             </h3>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-                <BikeCard />
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                {
+                   bikes.map(bike => <BikeCard 
+                    key={bike._id} 
+                    bike={bike}
+                    />) 
+                }
             </div>
         </div>
     );
