44604fbd32e15d70e5056b989d2a0943269b8057
Rakib099 <rakibnabi97@gmail.com>
2023-01-09 12:17:09 +0600

    Error page updated

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate, useRouteError } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
+import oops from '../../assets/images/404-error.png';
 
 const ErrorPage = () => {
   const { logOut } = useContext(AuthContext);
@@ -18,12 +19,16 @@ const ErrorPage = () => {
 
   return (
     <div id="error" className="text-center h-64 flex flex-col justify-center">
-      <h2 className='font-semibold text-[teal] text-2xl'>Something went wrong!!!</h2>
-      <p className='my-2'>
+      <div className="avatar">
+        <div className="w-32 rounded mx-auto">
+          <img src={oops} alt="Error" />
+        </div>
+      </div>
+      <p className='mb-5'>
         <span className='text-red-400'>{error.status} </span>
         <i className='opacity-75'>{error.statusText || error.message}</i>
       </p>
-      <p>Return to <Link to='/' className='font-semibold text-indigo-400'>Home</Link> <br /> or <button onClick={handleLogout} className='btn btn-secondary btn-xs text-white'>Log out</button> and <Link to='/login' className='font-semibold text-blue-400 hover:text-blue-500'>Login</Link> again.</p>
+      <p>Return to <Link to='/' className='font-semibold text-indigo-400'>Home</Link> or <button onClick={handleLogout} className='btn btn-secondary btn-xs text-white'>Log out</button> and <Link to='/login' className='font-semibold text-blue-400 hover:text-blue-500'>Login</Link> again.</p>
     </div>
   );
 };
